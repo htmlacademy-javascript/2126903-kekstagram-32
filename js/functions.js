a54f08d3c3541ea1f59eacac82b7a1a20bb0d773
simon <semyon.dudin@inbox.ru>
2024-07-11 20:30:01 +0300

    Фукции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,7 @@
 // Задание 1
 
 const checkLength = (string, length) => string.length <= length;
-checkLength();
-// console.log(checkLength('проверяемая строка', 20)); // true
+console.log(checkLength('проверяемая строка', 20)); // true
 // console.log(checkLength('проверяемая строка', 18)); // true
 // console.log(checkLength('проверяемая строка', 10)); // false
 
@@ -20,8 +19,7 @@ const checkPalindrom = (string) => {
   }
   return false;
 };
-checkPalindrom();
-// console.log(checkPalindrom('топот')); // true
+console.log(checkPalindrom('топот')); // true
 // console.log(checkPalindrom('ДовОд')); // true
 // console.log(checkPalindrom('Кекс')); // false
 // console.log(checkPalindrom('Лёша на полке клопа нашёл')); // true
@@ -38,6 +36,33 @@ const parseNumber = (string) => {
   }
   return stringNumber;
 };
-parseNumber();
-// console.log(parseNumber('3222 какой - то левый текст')); // 3222
+
+console.log(parseNumber('3222 какой - то левый текст')); // 3222
 // console.log(parseNumber('3222 какой 777 то левый 34 текст')); // 322277734
+
+// Задание 5.2
+
+
+const splitTime = (time) => {
+  const timeArray = time.split(':');
+  const hourToMinute = () => {
+    let workInMinute = 0;
+    for (let i = 0; i < timeArray.length; i += 2) {
+      workInMinute = timeArray[i] * 60;
+      for (i = 1; i < timeArray.length; i += 2) {
+        workInMinute += +timeArray[i];
+      }
+    }
+    return workInMinute;
+  };
+  return hourToMinute;
+};
+
+const checkTime = (startWorkDay, endWorkDay, startMeet, timeMeet) => splitTime(startMeet) + timeMeet <= splitTime(endWorkDay) && splitTime(startMeet) + timeMeet >= splitTime(startWorkDay);
+
+console.log(checkTime('08:00', '17:30', '14:00', 90)); // true
+console.log(checkTime('8:0', '10:0', '8:0', 120)); // true
+console.log(checkTime('08:00', '14:30', '14:00', 90)); // false
+console.log(checkTime('14:00', '17:30', '08:0', 90)); // false
+console.log(checkTime('8:00', '17:30', '08:00', 900)); // false
+
