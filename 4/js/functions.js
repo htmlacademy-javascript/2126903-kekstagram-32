6ae4f57dc5e99e9e15037eda9341c04bce0f01bd
Keks <keks@htmlacademy.ru>
2024-07-11 18:24:18 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,43 @@
+// Задание 1
+
+const checkLength = (string, length) => string.length <= length;
+checkLength();
+// console.log(checkLength('проверяемая строка', 20)); // true
+// console.log(checkLength('проверяемая строка', 18)); // true
+// console.log(checkLength('проверяемая строка', 10)); // false
+
+// Задание 2
+
+const checkPalindrom = (string) => {
+  string = string.replaceAll(' ', '');
+  let stringReverse = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    stringReverse += string[i];
+  }
+
+  if (string.toUpperCase() === stringReverse.toUpperCase()) {
+    return true;
+  }
+  return false;
+};
+checkPalindrom();
+// console.log(checkPalindrom('топот')); // true
+// console.log(checkPalindrom('ДовОд')); // true
+// console.log(checkPalindrom('Кекс')); // false
+// console.log(checkPalindrom('Лёша на полке клопа нашёл')); // true
+
+// Задание 3
+
+
+const parseNumber = (string) => {
+  let stringNumber = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(Number(string[i])) && string[i] !== ' ') {
+      stringNumber += string[i];
+    }
+  }
+  return stringNumber;
+};
+parseNumber();
+// console.log(parseNumber('3222 какой - то левый текст')); // 3222
+// console.log(parseNumber('3222 какой 777 то левый 34 текст')); // 322277734
